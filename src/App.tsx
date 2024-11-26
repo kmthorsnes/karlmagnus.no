ba7aaa5cadf070199ea704d3154ae9eebc4a10b8
Karl-Magnus Thorsnes <kalle@Karl-Magnus-sin-MacBook-Pro.local>
2024-11-26 23:41:36 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hei! Jeg lager internett-ting</h1>
+      <p className="read-the-docs">
+Send meg en <a href="mailto:hei@karlmagnus.no">epost</a>      </p>
+    </>
+  )
+}
+
+export default App
