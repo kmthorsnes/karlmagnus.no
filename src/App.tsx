252f9056138ca379457d2ba060e050f44259c661
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-05-05 12:30:01 +0200

    Add emoji

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Hei! Jeg lager internett-ting</h1>
+      <h1>Hei!👋🏼️Jeg lager internett-ting</h1>
       <p className="read-the-docs">
-Send meg en <a href="mailto:hei@karlmagnus.no">epost</a>      </p>
+        Send meg en <a href="mailto:hei@karlmagnus.no">epost</a>{" "}
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
