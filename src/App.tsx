79a1e0bbc779b6095ce8633aabf17297021cceb3
Karl-Magnus Thorsnes <kalle@Karl-Magnus-sin-MacBook-Pro.local>
2025-08-09 20:37:47 +0200

    Legg til tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Hei! Jeg lager internett-ting</h1>
-      <p className="read-the-docs">
-Send meg en <a href="mailto:hei@karlmagnus.no">epost</a>      </p>
+      <h1 className="text-3xl font-bold pb-4">
+        Hei! 👋 Jeg lager internett-ting
+      </h1>
+      <p className="text-lg">
+        Send meg en <a href="mailto:hei@karlmagnus.no">epost</a>{" "}
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
