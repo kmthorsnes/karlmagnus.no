acc4907a293eef43c902863a9eadc194dbb08e64
Karl-Magnus Thorsnes <kalle@Karl-Magnus-sin-MacBook-Pro.local>
2025-08-09 23:32:35 +0200

    kjøh

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { tanStackRouter } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tanStackRouter({ autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
