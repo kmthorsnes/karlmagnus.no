3816545958a6397854f7e5fcd58fc47f94350689
Karl-Magnus Thorsnes <kalle@Karl-Magnus-sin-MacBook-Pro.local>
2025-08-09 22:04:35 +0200

    jada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
-import logo from '../logo.svg'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -7,16 +6,16 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-centern">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+    <div className="text-center">
+      <main className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
         <h1>Hei!👋🏼️Jeg lager internett-ting</h1>
         <p className="">
           Send meg en <a href="mailto:hei@karlmagnus.no">epost</a>{' '}
         </p>
         <p>
-          Eller les om ting jeg <Link className='text-red-600 underline' to="/liker">liker</Link>
+          Eller les om ting jeg <Link to="/liker">liker</Link>
         </p>
-      </header>
+      </main>
     </div>
   )
 }
--- src/routes/liker.tsx
+++ src/routes/liker.tsx
@@ -1,9 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Table, TableHeader, TableRow, TableHead } from 'lucide-react'
 
 export const Route = createFileRoute('/liker')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Dette er en liste over ting jeg liker:</div>
+  return     <div className="text-center">
+  <main className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+    <h1>Dette er ting jeg liker</h1>
+<Table>
+  <TableHeader>
+    <TableRow>
+      <TableHead>Ting</TableHead>
+    </TableRow>
+  </TableHeader>
+</Table>
+  </main>
+</div>
 }
